dd2dbb198b57a8a2e56595a932a83806ef6f883f
LysandrosAn <anastasopoulos_l@mail.com>
2021-03-07 15:21:05 +0100

    Corrections

---
+++ src/Flywheel_statespace.jl
@@ -0,0 +1,12 @@
+# ======================== Create matrices  ======================== #
+function Flywheel_statespace(RotorSpreadsheet,Omega)
+  t1=println("Creating state-space dynamic matrix...")
+
+ M,G,C,K =Flywheel_FEMatrices(RotorSpreadsheet)
+ N,NN,NNN,len,ro,ri,rho,E,nu,It,A,mu,jp,jt,PosNN,BearX,BearY,PosNNN,DiscThick,DiscRad =Flywheel_load(RotorSpreadsheet)
+
+ # Allocate arrays
+ DynMa=  zeros(Float64,8*(N+1),8*(N+1));
+ 
+ return DynMa
+end    # Flywheel_statespace()
