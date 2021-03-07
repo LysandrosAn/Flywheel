723b27af6ca9ece589352f46824c3b05bc55e7d6
LysandrosAn <anastasopoulos_l@mail.com>
2021-03-07 10:14:45 +0100

    Add references

--- src/Flywheel_localize.jl
+++ src/Flywheel_localize.jl
@@ -4,9 +4,6 @@ function Flywheel_localize(k,n,r)
   for i=1:r
     Q[i,4*(k-1)+i]=1
   end
-  # k: node position
-  # n: overall amount of nodes
-  # r: amount of displacements affected (8 for elements, 4 for translational bearings
  return Q
 end    # Flywheel_localize(k,n,r)
 
