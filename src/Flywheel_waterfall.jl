041e01977b1cb7684497920a603c4493e0b0a3ae
LysandrosAn <anastasopoulos_l@mail.com>
2021-03-09 12:53:44 +0100

    Added Campbell

--- src/Flywheel_waterfall.jl
+++ src/Flywheel_waterfall.jl
@@ -1,20 +1,20 @@
 # ======================== Create Campbell diagram  ======================== #
-
 function Flywheel_waterfall(M,G,D,K,OmStart,OmEnd)
   t1=println("Generating Campbell diagram...")
   N=Int64(size(M)[1]/4-1)
+  OmVec=  zeros(Float64,8*(N+1));
 
   p2=0
   plot()
 
-  for Om=collect(OmStart:100:OmEnd)
-    A,B=Flywheel_statespace(M,G,D,K,Om)
-    v=sort(abs.(imag(eigvals(A)*60/2/pi)))
-    for k=1:Int64(length(v))
-      p2=plot!( Om, imag(v[k]), marker = ([:circle :d]), legend=:false)
-    end
+  Omega= collect(OmStart:1000:OmEnd)
+  for (Omindex, Omvalue) in enumerate(Omega)
+     A,B=Flywheel_statespace(M,G,D,K,Omvalue)
+     v=sort(abs.(imag(eigvals(A)*60/2/pi)))
+    for k=1:8*(N+1)
+      OmVec[k]=Omvalue
+    end  
+    p2=plot!(OmVec,v, marker = ([:circle],  2, 0.8),linealpha=0.0, legend=:false,xlims = (OmStart*0.75, OmEnd*1.25), yscale = :log, ylims = (1e2, +1e5), markercolor = :blue, xlabel= "Rotational speed, rev/min", ylabel = "Eigenfrequeny, rev/min")
   end
-
   return p2
-
- end    # Flywheel_waterfall()
+end    # Flywheel_waterfall()
