fc3bd57d81b80d261b65526afed83c24bb2389ad
LysandrosAn <anastasopoulos_l@mail.com>
2021-03-09 18:13:15 +0100

    Add modal

--- src/Flywheel_modes.jl
+++ src/Flywheel_modes.jl
@@ -1,7 +1,8 @@
 # ======================== Visualize vibration modes  ======================== #
-function Flywheel_modes(RotorSpreadsheet,AA)
+function Flywheel_modes(RotorSpreadsheet,AA,modeno)
   t1=println("Showing vibration mode...")
   N,NN,NNN,len,ro,ri,rho,E,nu,It,A,mu,jp,jt,PosNN,BearX,BearY,PosNNN,adro,adri,adle,adrho,adma,adjp,adjt,DiscThick,DiscRad =Flywheel_load(RotorSpreadsheet)
+  Flywheel_blueprint(RotorSpreadsheet)
   nodes_trX= zeros(Int64,N+1,1)
   nodes_trY= zeros(Int64,N+1,1)
   
@@ -11,22 +12,20 @@ function Flywheel_modes(RotorSpreadsheet,AA)
   d=(abs.(imag(dDynMa)))
   dind=sortperm(abs.(imag(dDynMa)))
 
-  println(string("1st Eigenvalue:",d[dind[1]]))
-  println(string("2nd Eigenvalue:",d[dind[2]]))
-  println(string("3rd Eigenvalue:",d[dind[3]]))
-  println(string("4th Eigenvalue:",d[dind[4]]))
-
   for ii=1:N+1
    nodes_trX[ii]=2*(ii-1)+1
    nodes_trY[ii]=2*(ii-1)+1+2*(N+1)
   end
 
-  mode_1=vDynMa[:,dind[1]]
-  println(imag.(mode_1))
+  println(string("Eigenvalue No:",modeno,", at: ",round(d[dind[4]],digits=2),"rad/s"))
+  mode_1=vDynMa[:,dind[modeno]]
   p=0
 #  plot()
-  p=plot!([0.0; cumsum(len)], 1e4*real.(mode_1[nodes_trY]), label="Modal shape, real",       linewidth = 1.75)
-  p=plot!([0.0; cumsum(len)], 1e4*imag.(mode_1[nodes_trY]), label="Modal shape, imaginary",  linewidth = 1.75)
+  ScaleFacRe=maximum(DiscRad)/maximum(abs.(real.(mode_1[nodes_trY])))
+  ScaleFacIm=maximum(DiscRad)/maximum(abs.(imag.(mode_1[nodes_trY])))
+
+  p=plot!([0.0; cumsum(len)], ScaleFacRe*real.(mode_1[nodes_trY]), label="Normal modal, real part",       linewidth = 1.75)
+  p=plot!([0.0; cumsum(len)], ScaleFacRe*imag.(mode_1[nodes_trY]), label="Normal modal, imaginary part",       linewidth = 1.75)
    return p
 
 end    # Flywheel_modes()
