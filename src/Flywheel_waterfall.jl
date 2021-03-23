4ac07ac32e044b341c0a09fbae3eaf5f999362ec
LysandrosAn <anastasopoulos_l@mail.com>
2021-03-23 19:05:58 +0100

    Added 1xOmega

--- src/Flywheel_waterfall.jl
+++ src/Flywheel_waterfall.jl
@@ -1,6 +1,6 @@
 # ======================== Create Waterfall diagram  ======================== #
 # Author: Lysandros Anastasopoulos
-function Flywheel_waterfall(M,G,D,K,OmStart,OmEnd)
+function Flywheel_waterfall(M,G,D,K,OmStart,steps,OmEnd)
   t1=println("Generating Campbell diagram...")
   N=Int64(size(M)[1]/4-1)
   OmVec=  zeros(Float64,8*(N+1));
@@ -8,14 +8,16 @@ function Flywheel_waterfall(M,G,D,K,OmStart,OmEnd)
   p2=0
   plot()
 
-  Omega= collect(OmStart:1000:OmEnd)
+  Omega= collect(OmStart:steps:OmEnd)
   for (Omindex, Omvalue) in enumerate(Omega)
      A,B=Flywheel_statespace(M,G,D,K,Omvalue)
-     v=sort(abs.(imag(eigvals(A)*60/2/pi)))
+     v=sort(abs.(imag(eigvals(A))))
     for k=1:8*(N+1)
       OmVec[k]=Omvalue
     end  
-    p2=plot!(OmVec,v, marker = ([:circle],  2, 0.8),linealpha=0.0, legend=:false,xlims = (OmStart*0.75, OmEnd*1.25), yscale = :log, ylims = (1e2, +1e5), markercolor = :blue, xlabel= "Rotational speed, rev/min", ylabel = "Eigenfrequeny, rev/min")
+    p2=plot!(OmVec,v, marker = ([:circle],  2, 0.8),linealpha=0.0, legend=:false,xlims = (OmStart*0.75, OmEnd*1.25), yscale = :log, markercolor = :blue, xlabel= "Rotational speed, rev/min", ylabel = "Eigenvalue (imag. part), rad/s")
   end
+  p2=plot!([OmStart OmEnd],[OmStart OmEnd]/60*2*pi,linewidth = 1.75, linecolor = :indianred4, legend=:false)
+
   return p2
 end    # Flywheel_waterfall()
